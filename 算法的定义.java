b65f73ba8f6d11b072965cdfd4901d79e73ad203
Yubao-wss <43661069+Yubao-wss@users.noreply.github.com>
2019-01-23 23:48:56 +0800

    Create 算法的定义.java

---
+++ 算法的定义.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //计算1~100的和
+        int tatal=0;
+        int end=100;
+
+        //使用for循环计算
+        for (int i=1;i<=end;i++){
+            tatal += i;
+        }
+
+        //直接计算
+        tatal=(1+end)*end/2;
+        //数据结构+算法=程序
+        //算法没有最好的，只有最合适的
+    }
+}
