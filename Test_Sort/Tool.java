f415e56405bf67b07874f8b029a29ee6490dd70e
Yubao-wss <43661069+Yubao-wss@users.noreply.github.com>
2019-03-15 23:44:02 +0800

    工具类，用于测试算法

--- Test_Sort/Tool.java
+++ Test_Sort/Tool.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * @Author: WSS
  * @Date:
@@ -16,5 +18,46 @@ public class Tool {
 
         }
         System.out.print("]");
+    }
+
+    /***
+     * 自定义一个随机数组
+     * @param size 数组长度
+     * @param min 最小值
+     * @param max 最大值
+     * @return
+     */
+    public static int[] creatArray(int size,int min,int max){
+        Random random = new Random();
+        int[] newArray = new int[size];
+        for (int i = 0;i < size;i++){
+            int number = min+random.nextInt(max+1);
+            newArray[i] = number;
+        }
+        return newArray;
+    }
+
+    /***
+     * 生成一个近似有序数组
+     * @param n 要生成的数据大小
+     * @param switchTimes 交换次数
+     * @return
+     */
+    public static int[] generateNearlySortedArray(int n,int switchTimes){
+        int[] result = new int[n];
+        //先构造一个完全有序的数据集
+        for (int i = 0;i<n;i++){
+            result[i] = i;
+        }
+        for(int i = 0;i < switchTimes;i++){
+            //随机选取范围内的两个索引下标进行交换
+            Random random = new Random();
+            int a = random.nextInt(n);
+            int b = random.nextInt(n);
+            int temp = result[a];
+            result[a] = result[b];
+            result[b] = temp;
+        }
+        return result;
     }
 }
